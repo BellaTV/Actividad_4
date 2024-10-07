bbd3b954d97cb0010c153e4623ffcfaf22c26e2d
isabelaTV <isabellactv96@gmail.com>
2024-10-06 21:12:38 -0500

    se agregan cambios

--- src/components/SeeTask.jsx
+++ src/components/SeeTask.jsx
@@ -1,24 +1,34 @@
 import PropTypes from "prop-types";
 
-const SeeTask = ({ listTask }) => {
+
+
+
+const SeeTask = ({ listTask, onDone }) => {
     const elements = listTask.map((item, index) => (
         <li key={index}>
             <h2>{item.title || "Sin título"}</h2>
-            <p>{item.description || "Sin descripción"}</p>
+            <p>{item.description || "Sin descripción"} </p>
+            <hr />
         </li>
     ));
 
-    return (
-        <div>
-            <h1>Lista de tareas</h1>
 
+
+    return (
+        <div className="see-task">
+            <h1>Lista de tareas</h1>
+            <form>
             <ul>{elements}</ul>
+            </form>
         </div>
     );
 };
 
 SeeTask.propTypes = {
     listTask: PropTypes.array.isRequired,
+    deleteTask: PropTypes.func.isRequired,
+    completeTask: PropTypes.func.isRequired,
+    editTask: PropTypes.func.isRequired,
 };
 
 export default SeeTask;
